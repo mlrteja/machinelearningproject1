79cd22f1b5340c0abb8555a4a2f87b84997fa8cd
mlrt <lakshmi.ravi3003@gmail.com>
2023-04-15 17:27:35 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
 from setuptools import find_packages,setup 
 from typing import List
-hyphen_e_dot='-e .'
-def get_requirements(file_path:str)->List[str] :
-    """
-    This function will return the list of requirements    
-    """
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-        if hyphen_e_dot in requirements:
-            requirements.remove(hyphen_e_dot)
-    return requirements
-     
+
+PROJECT_NAME = "Machine Learning Project"
+VERSION = "0.0.1"
+AUTHOR = "mlraviteja"
+DESCRIPTION = "This is our machine learning pipeline building sesstion"
+HYPHEN_E_DOT = "-e ."
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+def get_requirements()->List[str] :
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list=requirement_file.readlines()
+        requirement_list=[requirement_name.replace("\n","") for requirement_name in requirement_list]
+
+
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
 
 
   
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='mlraviteja',
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
     author_email='lakshmi.ravi3003@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements_list()
 )
